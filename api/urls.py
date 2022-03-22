d1f495e4f0662e443255b3b4202c7ee1baa9b84c
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-22 20:54:44 +0530

    add update user endpoint

--- api/urls.py
+++ api/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('register', AuthUserRegistrationView.as_view(), name='register'),
     path('login', AuthUserLoginView.as_view(), name='login'),
     path('users', UserListView.as_view(), name='users'),
+    path('users/<int:pk>/', UserListView.as_view(), name='update user'),
     path('appointments', AppointmentListView.as_view(), name='appointments'),
     path('appointments/<int:pk>/', AppointmentListView.as_view(), name='appointment'),
     path('hospitals', HospitalListView.as_view(), name='hospitals'),
