3f8e174327ffd9771c85675782bdbc3a2053d547
sneaky-potato <rajivkamal.im421@gmail.com>
2022-03-25 16:16:44 +0530

    refactor urls

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
+from api.serializers import DoctorListAdminSerializer, DoctorListSerializer
 from rest_framework_simplejwt import views as jwt_views
 
 from .views import (
     AuthUserRegistrationView,
     AuthUserLoginView,
-    UserListView,
+    DoctorListView,
+    # Ping,
+    MeView,
+    UserEditView,
+    # UserListView,
     HospitalListView,
     AppointmentListView,
 )   
@@ -14,8 +19,12 @@ urlpatterns = [
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register', AuthUserRegistrationView.as_view(), name='register'),
     path('login', AuthUserLoginView.as_view(), name='login'),
-    path('users', UserListView.as_view(), name='users'),
-    path('users/<int:pk>/', UserListView.as_view(), name='update user'),
+
+    # path('ping', Ping.as_view(), name='ping'),
+    path('me', MeView.as_view(), name='me'),
+
+    path('doctors', DoctorListView.as_view(), name='users'),
+    path('users/<int:pk>/', UserEditView.as_view(), name='update user'),
     path('appointments', AppointmentListView.as_view(), name='appointments'),
     path('appointments/<int:pk>/', AppointmentListView.as_view(), name='appointment'),
     path('hospitals', HospitalListView.as_view(), name='hospitals'),
